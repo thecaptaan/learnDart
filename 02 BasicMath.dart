8e1319a391eeb31740033ebc4dd9aa2dec8e8e81
The Captaan <thecaptaan@outlook.com>
2024-06-13 12:08:25 +0530

    Basic maths

---
+++ 02 BasicMath.dart
@@ -0,0 +1,10 @@
+// Basic Math
+void main() {
+  print(4 + 2); // 6
+  print(4 - 2); // 2
+  print(4 * 2); // 8
+  print(4 / 2); // 2.0
+  print(4 % 2); // 0
+  print(4 ~/ 2); // 2
+  print(4 + 2 * 3); // 10
+}
