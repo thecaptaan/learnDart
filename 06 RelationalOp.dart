12e4b9bb4cad3ef9400cee6f8991046f4c7be921
The Captaan <thecaptaan@outlook.com>
2024-06-14 13:14:30 +0530

    Relational Operators

---
+++ 06 RelationalOp.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 10;
+  int b = 20;
+  print(a > b); // false
+  print(a < b); // true
+  print(a >= b); // false
+  print(a <= b); // true
+  print(a == b); // false
+  print(a != b); // true
+}
