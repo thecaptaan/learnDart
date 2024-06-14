f2b053e0a166b8055d27f23fb41503e7a838c51c
The Captaan <thecaptaan@outlook.com>
2024-06-14 23:45:53 +0530

    Null Saftey

--- 15 Null.dart
+++ 15 Null.dart
@@ -9,6 +9,10 @@ void main() {
   print(channel.money);
   print(channel.subscribers);
   channel.money = 100;
+
   // null check operator
   print(channel.money!);
+
+  int earning = channel.money ?? 0;
+  print(earning);
 }
