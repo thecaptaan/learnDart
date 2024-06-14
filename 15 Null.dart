44d238042135fafbc2207d3ced0470288aa99a61
The Captaan <thecaptaan@outlook.com>
2024-06-14 23:42:35 +0530

    Null Saftey

---
+++ 15 Null.dart
@@ -0,0 +1,14 @@
+class Channel {
+  bool isMonetized = false;
+  dynamic money = null;
+  int? subscribers = null;
+}
+
+void main() {
+  var channel = Channel();
+  print(channel.money);
+  print(channel.subscribers);
+  channel.money = 100;
+  // null check operator
+  print(channel.money!);
+}
