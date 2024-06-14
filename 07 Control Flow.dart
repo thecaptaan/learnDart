90de3998a1c14d7607c528bc5745e59ce6323871
The Captaan <thecaptaan@outlook.com>
2024-06-14 13:24:42 +0530

    if - elseif - else

---
+++ 07 Control Flow.dart
@@ -0,0 +1,19 @@
+void main() {
+  bool isWeatherGood = true;
+  if (isWeatherGood) {
+    print("Let's go out for a walk");
+  } else {
+    print("Let's stay at home");
+  }
+
+  // nested if-elseif-else
+  int a = 10;
+  int b = 20;
+  if (a > b) {
+    print("$a is greater than $b");
+  } else if (a < b) {
+    print("$a is less than $b");
+  } else {
+    print("$a is equal to $b");
+  }
+}
